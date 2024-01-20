4ff0150ab31a5d757c074a46b57bbf44751abb5d
dayoonasanya <aonasanya@mail.dccc.edu>
2024-01-19 17:39:15 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,20 +13,25 @@
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
+    bool swapped;
 
 	if (!array)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+        swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap_func(&array[j], &array[j + 1]);
+                swapped = true;
 				print_array(array, size);
 			}
 		}
+        if (!swapped)
+          break;
 	}
 }
 
