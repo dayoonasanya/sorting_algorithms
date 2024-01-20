94320b359be7e941ed9281579e9a25303e89cfb3
dayoonasanya <aonasanya@mail.dccc.edu>
2024-01-20 08:55:47 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,12 +26,12 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				swap_func(&array[j], &array[j + 1]);
-                swapped = true;
+				swapped = true;
 				print_array(array, size);
 			}
 		}
-        if (!swapped)
-          break;
+	if (!swapped)
+		 break;
 	}
 }
 
--- sort.h
+++ sort.h
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include "sort.h"
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
